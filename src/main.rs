509d39c5076f2608798de5c6b888e0fcaa0e562c
Fernando Filho <fernando@dimap.ufrn.br>
2021-02-24 09:37:14 -0300

    implementando endpoints ping e hello

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,24 @@
 use actix_web::{web, App, HttpRequest, HttpServer, Responder};
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
+// funções para cada rota (endpoint)
+async fn ping() -> impl Responder {
+    format!("pong")
+    //HttpResponse::Ok()
+}
+
+async fn hello(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("Mundo");
+    format!("Olá {}!", &name)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
+            .route("/ping", web::get().to(ping))
+            .route("/hello", web::get().to(hello))
+            .route("/{name}", web::get().to(hello))
+            .route("/", web::get().to(hello))
     })
     .bind("127.0.0.1:8000")?
     .run()
