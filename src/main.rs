f23e742648bdf769663d79b2a76a6bd573a5b6de
Fernando Filho <fernando@dimap.ufrn.br>
2021-02-28 15:25:36 -0300

    implementando o ping

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,8 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 
 // funções para cada rota (endpoint)
 async fn ping() -> impl Responder {
-    format!("pong")
-    //HttpResponse::Ok()
-}
-
-async fn hello(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("Mundo");
-    format!("Olá {}!", &name)
+    HttpResponse::Ok().finish()
 }
 
 #[actix_web::main]
@@ -16,9 +10,6 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .route("/ping", web::get().to(ping))
-            .route("/hello", web::get().to(hello))
-            .route("/{name}", web::get().to(hello))
-            .route("/", web::get().to(hello))
     })
     .bind("127.0.0.1:8000")?
     .run()
