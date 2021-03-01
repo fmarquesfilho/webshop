834a60441bb9802e42ecb9617fcc021f558ae696
Fernando Filho <fernando@dimap.ufrn.br>
2021-02-28 21:09:55 -0300

    enxugando lib.rs para separar os arq

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,4 @@
 //! src/lib.rs
-
-use actix_web::{web, App, HttpResponse, HttpServer};
-use actix_web::dev::Server;
-
-async fn ping() -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
-
-#[derive(serde::Deserialize)]
-pub struct FormData {
-    email: String,
-    name: String
-}
-
-async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
-
-// Notem a assinatura diferente da função (adicionamos pub)
-// Nós retornamos a variável 'Server' quando dá tudo certo e não usamos mais o async 
-// Não há mais a chamada para .async, pois não é mais necessária.
-pub fn run() -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
-            App::new()
-                .route("/ping", web::get().to(ping))
-                .route("/subscriptions", web::post().to(subscribe))
-    })
-    .bind("127.0.0.1:8000")?
-    .run();
-    // Retiramos o .await
-    Ok(server)
-}
+pub mod configuration;
+pub mod routes;
+pub mod startup;
